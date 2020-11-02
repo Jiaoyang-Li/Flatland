9d3b47d7ba5e78d9bc5d1175ac461abb4c8a8103
nobodyczcz <jianshen858@gmail.com>
2020-11-02 21:43:09 +1100

    maybe fixed a bug caused some deadlock

--- CBS-corridor/CBSH-rect-cmake/SinglePlanning.cpp
+++ CBS-corridor/CBSH-rect-cmake/SinglePlanning.cpp
@@ -328,7 +328,7 @@ bool SIPP::search() // TODO: weighted SIPP
 
         if (constraintTable.get_latest_constrained_timestep(start_location) > 0)
         {
-            int t_max = agent.malfunction_left;
+            int t_max = agent.malfunction_left+1;
             while(!constraintTable.is_constrained(agent.agent_id, start_location, t_max, -1) &&
                 t_max <= constraintTable.length_max)
                 t_max++;
@@ -351,9 +351,9 @@ bool SIPP::search() // TODO: weighted SIPP
         }
         else //if on a cross, exit_heading is always accurate, as move forwad must be along agent's current heading.
             start->exit_loc = start_location + ml.moves_offset[start->exit_heading];
-        int t_max = agent.malfunction_left;
-        while(constraintTable.is_constrained(agent.agent_id, start->exit_loc, t_max, start_location) && t_max <= constraintTable.length_max)
-            t_max++;
+        int t_max = agent.malfunction_left+1;
+//        while(constraintTable.is_constrained(agent.agent_id, start->exit_loc, t_max, start_location) && t_max <= constraintTable.length_max)
+//            t_max++;
         start->interval = make_pair(0,t_max);
     }
     start->open_handle = open_list.push(start);
